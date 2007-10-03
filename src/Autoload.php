aac243fafd95d46d200435b64777431549e5316d
Greg Beaver <greg@chiaraquartet.net>
2007-10-03 23:38:06 +0000

    don't die on class_exists()/interface_exists()

--- src/Autoload.php
+++ src/Autoload.php
@@ -9,15 +9,25 @@ function PEAR2_Autoload($class)
         fclose($fp);
         require str_replace('_', '/', $class) . '.php';
         if (!class_exists($class, false) && !interface_exists($class, false)) {
-            die ((string) new Exception('Class ' . $class . ' was not present in ' .
+            die(new Exception('Class ' . $class . ' was not present in ' .
                 str_replace('_', '/', $class) . '.php (include_path="' . get_include_path() .
                 '") [PEAR2_Autoload version 1.0]'));
         }
         return true;
     }
-    die ((string) new Exception('Class ' . $class . ' could not be loaded from ' .
+    $e = new Exception('Class ' . $class . ' could not be loaded from ' .
         str_replace('_', '/', $class) . '.php, file does not exist (include_path="' . get_include_path() .
-        '") [PEAR2_Autoload version 1.0]'));
+        '") [PEAR2_Autoload version 1.0]');
+    $trace = $e->getTrace();
+    if (isset($trace[2]) && isset($trace[2]['function']) &&
+          in_array($trace[2]['function'], array('class_exists', 'interface_exists'))) {
+        return false;
+    }
+    if (isset($trace[1]) && isset($trace[1]['function']) &&
+          in_array($trace[1]['function'], array('class_exists', 'interface_exists'))) {
+        return false;
+    }
+    die ((string) $e);
 }
 
 // set up __autoload
